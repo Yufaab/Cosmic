b2d7d196166177ceab012363eeba1b4951a5013b
JazibEqbal <jazibeqbal0526@gmail.com>
2023-12-24 20:51:04 +0530

    setup, Home Page and Header

---
+++ src/components/Header.tsx
@@ -0,0 +1,50 @@
+import React from 'react'
+import { Navbar, Nav, Container } from "react-bootstrap";
+import { LinkContainer } from "react-router-bootstrap";
+import styles from '../styles/Header.module.css';
+
+const Header = () => {
+  return (
+    <header>
+    <Navbar className={styles.navbarHeader} bg="dark" variant="dark" expand="lg" collapseOnSelect>
+      <Container>
+        <LinkContainer to="/">
+          <Navbar.Brand className={styles.mainHead}>YuFaab</Navbar.Brand>
+        </LinkContainer>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="m-auto float-right flex">
+            <LinkContainer to="/">
+              <Nav.Link>
+                <i></i>Home
+              </Nav.Link>
+            </LinkContainer>
+            <LinkContainer to="/cart">
+              <Nav.Link>
+                <i className="fas fa-shopping-cart"></i>Cart
+              </Nav.Link>
+            </LinkContainer>
+            <LinkContainer to="/user/my/orders">
+              <Nav.Link>
+                <i className="fas fa-user"></i>My Orders
+              </Nav.Link>
+            </LinkContainer>
+            <LinkContainer to="/signup">
+              <Nav.Link>
+                <i className="fas fa-user"></i>Sign In
+              </Nav.Link>
+            </LinkContainer>
+            <LinkContainer to="/login/admin">
+              <Nav.Link>
+                <i className="fas fa-user"></i>Admin
+              </Nav.Link>
+            </LinkContainer>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  </header>
+  )
+}
+
+export default Header
---
+++ src/pages/Home.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import xx from '../utils/B303.svg'
+import styles from '../styles/Home.module.css'
+
+const Home = () => {
+  return (
+    <div className={styles.homeContainer}>
+      <div className={styles.leftSide}>
+      <h4>Welcome to <span className={styles.yuFaabText}>YuFaab</span></h4>
+        <button className={styles.getStartedButton}>Get Started</button>
+        <div className={styles.popupContainer}>
+          Register for Counseling
+        </div>
+      </div>
+      <div className={styles.rightSide}>
+        <img src={xx} alt="Your" />
+      </div>
+    </div>
+  )
+}
+
+export default Home
