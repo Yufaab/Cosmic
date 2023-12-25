7f60a7c6456c81bf9afab280c72a174ad016e98e
Fahad Ali <alifahad2210@gmail.com>
2023-12-25 11:42:56 +0530

    eslint + prettier configured

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,23 @@
 import React from 'react'
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { Container } from "react-bootstrap";
-import Header from './components/Header';
-import Home from './pages/Home';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { Container } from 'react-bootstrap'
+import Header from './components/Header'
+import Home from './pages/Home'
 
 const App = () => {
   return (
     <Router>
-        <div>
-          <Header />
-          <main className="py-3">
-            <Container>
-              <Routes>
-                <Route path="/" element={<Home />} />
-              </Routes>
-            </Container>
-          </main>
-        </div>
-      </Router>
+      <div>
+        <Header />
+        <main className="py-3">
+          <Container>
+            <Routes>
+              <Route path="/" element={<Home />} />
+            </Routes>
+          </Container>
+        </main>
+      </div>
+    </Router>
   )
 }
 
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -6,11 +6,11 @@ const Home = () => {
   return (
     <div className={styles.homeContainer}>
       <div className={styles.leftSide}>
-      <h4>Welcome to <span className={styles.yuFaabText}>YuFaab</span></h4>
+        <h4>
+          Welcome to <span className={styles.yuFaabText}>YuFaab</span>
+        </h4>
         <button className={styles.getStartedButton}>Get Started</button>
-        <div className={styles.popupContainer}>
-          Register for Counseling
-        </div>
+        <div className={styles.popupContainer}>Register for Counseling</div>
       </div>
       <div className={styles.rightSide}>
         <img src={xx} alt="Your" />
